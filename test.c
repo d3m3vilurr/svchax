424bb9cf5307858f4730fb5508bcb1b4caac78b7
aliaspider <aliaspider@gmail.com>
2016-03-09 18:11:09 +0100

    remove the custom allocator, seems to be causing trouble with n3DS, and 3dsx loader keep 1MB of free memory anyway, so it isn't needed currently.

--- test.c
+++ test.c
@@ -4,35 +4,6 @@
 #include <malloc.h>
 #include "svchax.h"
 
-extern char* fake_heap_start;
-extern char* fake_heap_end;
-extern u32 __ctru_heap;
-extern u32 __ctru_heap_size;
-extern u32 __ctru_linear_heap;
-extern u32 __ctru_linear_heap_size;
-
-void __system_allocateHeaps(void)
-{
-   u32 tmp = 0;
-
-//    u32 size = (osGetMemRegionFree(MEMREGION_APPLICATION) / 2) & 0xFFFFF000;
-   __ctru_heap_size = 0x1000000;
-   __ctru_linear_heap_size = 0x1000000;
-
-   // Allocate the application heap
-   __ctru_heap = 0x08000000;
-   svcControlMemory(&tmp, __ctru_heap, 0x0, __ctru_heap_size, MEMOP_ALLOC, MEMPERM_READ | MEMPERM_WRITE);
-
-   // Allocate the linear heap
-   svcControlMemory(&__ctru_linear_heap, 0x0, 0x0, __ctru_linear_heap_size, MEMOP_ALLOC_LINEAR,
-                    MEMPERM_READ | MEMPERM_WRITE);
-
-   // Set up newlib heap
-   fake_heap_start = (char*)__ctru_heap;
-   fake_heap_end = fake_heap_start + __ctru_heap_size;
-
-}
-
 static const char* test_result ="FAILED!!";
 
 s32 set_test_result(void)
